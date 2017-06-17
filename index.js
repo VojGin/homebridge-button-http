900cfea07d3c31a7d29de13bf663cb6ffcf7119e
JonesfromHan <jonas-boeckmann@gmx.de>
2017-06-18 00:31:25 +0200

    Added code, updated package.json, removed tests
    Modified the dummy-plugin so it can be controlled via http. Updated the package.json to match the plugin. Removed the test.

--- index.js
+++ index.js
@@ -7,32 +7,51 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-switch-http", "HttpSwitch", HttpSwitch);
 }
 
-function DummySwitch(log, config) {
+function HttpSwitch(log, config) {
   this.log = log;
   this.name = config.name;
-  this.stateful = config.stateful;
+  this.port = config.port;
+  this.onStatus = false;
 
   this._service = new Service.Switch(this.name);
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
+	
+  var that = this;
+
+  var express = require('express');
+  var app = express();
+ 
+  
+  app.get('/set/1', function (req, res, next) {
+	  that.onStatus = true;
+	  that._service.setCharacteristic(Characteristic.On, true);
+	  res.sendStatus(200);
+	});
+
+	app.get('/set/0', function (req, res, next) {
+	  that.onStatus = false;
+	  that._service.setCharacteristic(Characteristic.On, false);
+	  res.sendStatus(200);
+	});
+
+	var server = app.listen(this.port, function () {
+	  var host = server.address().address;
+	  var port = server.address().port;
+	  console.log('app listening at', host, port);
+	});
 }
 
-DummySwitch.prototype.getServices = function() {
+HttpSwitch.prototype.getServices = function() {
   return [this._service];
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+HttpSwitch.prototype._setOn = function(on, callback) {
 
   this.log("Setting switch to " + on);
-
-  if (on && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), 1000);
-  }
 
   callback();
 }
