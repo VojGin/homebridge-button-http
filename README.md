bddf12918e6e0ea7d1ce0a291df84e9ae99067c3
Vojtěch Gintner <gintnervojtech@gmail.com>
2019-05-14 09:30:07 +0200

    button

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 
-# Plugin for controlling switches over http input
+# Plugin for controlling buttons over http input
 
 Example config.json:
 
 ```
     "accessories": [
         {
-          "accessory": "HttpSwitch",
-          "name": "Switch"
+          "accessory": "HttpButton",
+          "name": "Button"
           "port": 4321
         }   
     ]
 
 ```
 
-This plugin allows you to controll switches in Homebridge remotly via http. The plugin opens up a web server. The switch can be activated by calling
+This plugin allows you to controll buttons in Homebridge remotly via http. The plugin opens up a web server. The button can be activated by calling
 ```
 http://<server-ip>:port/set/1
 ```
@@ -23,18 +23,14 @@ and deactivated by calling
 http://<server-ip>:port/set/0
 ```
 
-For each instance of the plugin, so for each switch, an new server has to be opened on a different port, so set the ports accordingly in the config.json.
+For each instance of the plugin, so for each button, an new server has to be opened on a different port, so set the ports accordingly in the config.json.
 
 ### Installation
 To install this plugin on your machine, please do
 ```
-sudo npm install -g JonesfromHan/homebridge-switch-http
+sudo npm install -g VojGin/homebridge-button-http
 ```
 
 ### Use case
-I designed this plugin so you can use a regular switch connected to an Arduino, ESP32, ESP8266 or Raspberry Pi to controll a switch in Homebridge. If you use an Apple TV or iPad as a controller, you could set automatic to the switch, such as turning a lamp on and off.
+I designed this plugin so you can use a regular button connected to an Arduino, ESP32, ESP8266 or Raspberry Pi to controll a button in Homebridge. If you use an Apple TV or iPad as a controller, you could set automatic to the button, such as turning a lamp on and off.
 Of course, it is possible to adopt the plugin to other use cases.
-
-### TODO
-* Add an option to listen only to localhost
-* Other ideas?
--- index.js
+++ index.js
@@ -1,57 +1,49 @@
-"use strict";
+'use strict'
 
-var Service, Characteristic;
+var Service, Characteristic
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
 
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
 
-  homebridge.registerAccessory("homebridge-switch-http", "HttpSwitch", HttpSwitch);
+  homebridge.registerAccessory('homebridge-button-http', 'HttpButton', HttpButton)
 }
 
-function HttpSwitch(log, config) {
-  this.log = log;
-  this.name = config.name;
-  this.port = config.port;
-  this.onStatus = false;
+function HttpButton(log, config) {
+  this.log = log
+  this.name = config.name
+  this.port = config.port
+  this._service = new Service.StatelessProgrammableSwitch(this.name)
 
-  this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
-	
-  var that = this;
+  var that = this
 
-  var express = require('express');
-  var app = express();
- 
-  
+  var express = require('express')
+  var app = express()
+
+
+  app.get('/set/0', function (req, res, next) {
+    this._service.notifyCharacteristic(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS)
+    res.sendStatus(200)
+  })
+
   app.get('/set/1', function (req, res, next) {
-	  that.onStatus = true;
-	  that._service.setCharacteristic(Characteristic.On, true);
-	  res.sendStatus(200);
-	});
+    this._service.notifyCharacteristic(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS)
+    res.sendStatus(200)
+  })
 
-	app.get('/set/0', function (req, res, next) {
-	  that.onStatus = false;
-	  that._service.setCharacteristic(Characteristic.On, false);
-	  res.sendStatus(200);
-	});
+  app.get('/set/2', function (req, res, next) {
+    this._service.notifyCharacteristic(Characteristic.ProgrammableSwitchEvent.LONG_PRESS)
+    res.sendStatus(200)
+  })
 
-	var server = app.listen(this.port, function () {
-	  var host = server.address().address;
-	  var port = server.address().port;
-	  that.log('app listening at', host, port);
-	});
+  var server = app.listen(this.port, function () {
+    var host = server.address().address
+    var port = server.address().port
+    that.log('app listening at', host, port)
+  })
 }
 
-HttpSwitch.prototype.getServices = function() {
-  return [this._service];
-}
-
-HttpSwitch.prototype._setOn = function(on, callback) {
-
-  this.log("Setting switch to " + on);
-
-  callback();
+HttpButton.prototype.getServices = function () {
+  return [this._service]
 }
